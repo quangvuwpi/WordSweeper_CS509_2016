4cbdf49102263fc88de4f0f24b489fb711aabffb
quangvu <quangvuwpi@gmail.com>
2016-10-03 19:19:44 -0400

    clean up file

--- WordSweeper/src/app/boundary/BoardPanel.java
+++ WordSweeper/src/app/boundary/BoardPanel.java
@@ -13,7 +13,6 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
-import java.awt.event.MouseListener;
 
 public class BoardPanel extends JPanel {
 
