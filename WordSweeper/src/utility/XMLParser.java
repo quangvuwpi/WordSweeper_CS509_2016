6a1caef0abad40cb1c1f73ef22732dd7c8aa8d5c
quangvu <quangvuwpi@gmail.com>
2016-10-06 17:59:07 -0400

    XML parser in progress..

--- WordSweeper/src/utility/XMLParser.java
+++ WordSweeper/src/utility/XMLParser.java
@@ -16,43 +16,59 @@ import client.model.Position;
  *
  */
 public class XMLParser {
-	
+
 	/**
 	 * Parse an XML string and return a Position
 	 * 
-	 * @param xml the XML string
+	 * @param xml
+	 *            the XML string
 	 * @return the new Position, or null if string is invalid
 	 */
 	public static Position parseXmlPosition(String xml) {
 		if (xml != null && xml.length() == 3) {
-			String regex = "(\\d),(\\d)";		
+			String regex = "(\\d),(\\d)";
 			Matcher m = Pattern.compile(regex).matcher(xml);
-			
+
 			if (m.matches()) {
-				int col = Integer.valueOf(m.group(1)) - 1;  // Position is offset by 1
-				int row = Integer.valueOf(m.group(2)) - 1;  // Position is offset by 1
-				
+				int col = Integer.valueOf(m.group(1)) - 1; // Position is offset
+															// by 1
+				int row = Integer.valueOf(m.group(2)) - 1; // Position is offset
+															// by 1
+
 				return new Position(col, row);
 			}
 		}
 		return null;
 	}
-	
+
 	/**
 	 * Parse an XML string and return a Board
 	 * 
-	 * @param xml the XML string
+	 * @param xml
+	 *            the XML string
 	 * @return the new Board, or null if string is invalid
 	 */
 	public static Board parseXmlBoard(String xml) {
-		if (xml != null && xml.length() == 16) {
-			String regex = "([A-Z]{16})";		
+		if (xml != null && xml.length() >= 16) {
+			String regex = "[A-Z]*(Qu)*[A-Z]*";
 			Matcher m = Pattern.compile(regex).matcher(xml);
-			
+
 			if (m.matches()) {
-				return new Board();
+				// If XML length is greater than 16, there has to be a Qu
+				// somewhere
+				
+				
+				Matcher p = Pattern.compile("[A-Z]*Qu[A-Z]*").matcher(xml);
+				System.out.println(p.groupCount());
+				for (int i = 0; i < p.groupCount(); i++) {
+					System.out.println(p.group(i));
+				}
+				
+				if (xml.length() == 16 || (xml.length() > 16 && m.groupCount() >= 2)) {
+					return new Board();
+				}
 			}
-		}		
+		}
 		return null;
 	}
 
--- WordSweeper/test/utility/XMLParserTest.java
+++ WordSweeper/test/utility/XMLParserTest.java
@@ -13,25 +13,26 @@ import junit.framework.TestCase;
 public class XMLParserTest extends TestCase {
 
 	/**
-	 * Test method for {@link utility.XMLParser#parseXmlPosition(java.lang.String)}.
+	 * Test method for
+	 * {@link utility.XMLParser#parseXmlPosition(java.lang.String)}.
 	 */
 	public void testParseXmlPosition() {
 		// null case
 		String xml = null;
 		assertNull(XMLParser.parseXmlPosition(xml));
-		
+
 		// empty string case
 		xml = "";
 		assertNull(XMLParser.parseXmlPosition(xml));
-		
+
 		// short string case
 		xml = "1";
 		assertNull(XMLParser.parseXmlPosition(xml));
-		
+
 		// long string case
 		xml = "1234";
 		assertNull(XMLParser.parseXmlPosition(xml));
-		
+
 		// malformed string case
 		xml = "123";
 		assertNull(XMLParser.parseXmlPosition(xml));
@@ -43,18 +44,51 @@ public class XMLParserTest extends TestCase {
 		assertNull(XMLParser.parseXmlPosition(xml));
 		xml = "1,.";
 		assertNull(XMLParser.parseXmlPosition(xml));
-		
+
 		// success case
 		xml = "2,3";
-		Position p = new Position(1,2);  // remember the offset by 1...		
+		Position p = new Position(1, 2); // remember the offset by 1...
 		assertTrue(p.equals(XMLParser.parseXmlPosition(xml)));
 	}
 
 	/**
-	 * Test method for {@link utility.XMLParser#parseXmlBoard(java.lang.String)}.
+	 * Test method for
+	 * {@link utility.XMLParser#parseXmlBoard(java.lang.String)}.
 	 */
 	public void testParseXmlBoard() {
-		fail("Not yet implemented");
+		// null string case
+		String xml = null;
+		assertNull(XMLParser.parseXmlBoard(xml));
+
+		// empty string case
+		xml = "";
+		assertNull(XMLParser.parseXmlBoard(xml));
+
+		// short string case
+		xml = "ABCD";
+		assertNull(XMLParser.parseXmlBoard(xml));
+
+		// long string case
+		xml = "AKJKOJWOERLKDORHFILRNEBCD";
+		assertNull(XMLParser.parseXmlBoard(xml));
+
+		// malformed string case
+		xml = "ABCDEFGHI,KLMNOP";
+		assertNull(XMLParser.parseXmlBoard(xml));
+		xml = "ABCDEFdbHIKLMNOP";
+		assertNull(XMLParser.parseXmlBoard(xml));
+		xml = "ABCDEF1HIHKLMNOP";
+		assertNull(XMLParser.parseXmlBoard(xml));
+		
+		// success case
+		xml = "ABCDEFGHIJKLMNOP";
+		assertNotNull(XMLParser.parseXmlBoard(xml));
+		xml = "ABCDEQuGHIJKLMNOP";
+		assertNotNull(XMLParser.parseXmlBoard(xml));
+		xml = "ABCDEFGHIJKLMNOQu";
+		assertNotNull(XMLParser.parseXmlBoard(xml));
+		xml = "QuBCDEFGHIJKLMNOP";
+		assertNotNull(XMLParser.parseXmlBoard(xml));
 	}
 
 }
