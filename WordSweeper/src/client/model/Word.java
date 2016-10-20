a74ec80f54c1153b7dcb30e2944db4d04977ac14
quangvu <quangvuwpi@gmail.com>
2016-10-20 00:06:39 -0400

    Implement Iterator interface

--- WordSweeper/src/client/model/Word.java
+++ WordSweeper/src/client/model/Word.java
@@ -1,14 +1,16 @@
 package client.model;
 
-import java.util.Stack;
+import java.util.Iterator;
+import java.util.LinkedList;
 
-public class Word {
-	Stack<Cell> cell;
-	int point;
+public class Word implements Iterator<Cell> {
+	protected int current = 0;
+	public LinkedList<Cell> cell;
+	public int point;
 
     public Word(){
     	this.point = 0;
-    	this.cell = new Stack<Cell>();
+    	this.cell = new LinkedList<Cell>();
     }
 
     public boolean addCell(Cell p) {
@@ -18,5 +20,19 @@ public class Word {
     	}
     	return false;
     }
+
+	@Override
+	public boolean hasNext() {
+		return (current < cell.size());
+	}
+
+	@Override
+	public Cell next() {
+		if (hasNext()) {
+			current++;
+			return cell.iterator().next();
+		}		
+		return null;
+	}
 
 }
