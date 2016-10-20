bb14f53ba04d22f13fb7e4e20973d7f941597db0
quangvu <quangvuwpi@gmail.com>
2016-10-19 23:54:26 -0400

    Modify Board class to be more flexible

--- WordSweeper/src/client/model/Board.java
+++ WordSweeper/src/client/model/Board.java
@@ -8,56 +8,77 @@ package client.model;
  *
  */
 public class Board {
-	
+
 	public static int COL_COUNT = 4;
 	public static int ROW_COUNT = 4;
-	
+
 	/**
 	 * Array of cells
 	 */
-	protected Cell cells[][] = new Cell[ROW_COUNT][COL_COUNT];
+	protected Cell cells[][] = new Cell[COL_COUNT][ROW_COUNT];
 	
 	/**
-	 * Initialize the board
-	 * 
-	 * Assuming the letters come in row-by-row
-	 * 
-	 * @param letters the letters to fill the board with
+	 * Empty board constructor
 	 */
-	public Board(String letters[][]) {
-		int mul = 0;
+	public Board() {
 		for (int row = 0; row < ROW_COUNT; row++) {
 			for (int col = 0; col < COL_COUNT; col++) {
-				Cell c   = new Cell(new Position(col,row));
-				c.letter = letters[col][row];
-				c.point  = 1;
-				c.multiplier = mul;
-				mul++;
-				
-				cells[col][row] = c;
+				this.cells[col][row] = new Cell(new Position(col, row));
 			}
 		}
 	}
-	
+
+	/**
+	 * Initialize the board
+	 * 
+	 * Assuming the cells come in row-by-row
+	 * 
+	 * @param cells
+	 *            the cells to fill the board with
+	 */
+	public Board(Cell cells[][]) {
+		for (int row = 0; row < ROW_COUNT; row++) {
+			for (int col = 0; col < COL_COUNT; col++) {
+				this.cells[col][row] = cells[col][row];
+			}
+		}
+	}
+
 	public Cell getCell(Position p) {
 		if (p != null) {
 			return cells[p.col][p.row];
 		}
 		return null;
 	}
-	
+
 	public void cancel() {
-		for(Cell[] i: cells){
-			for(Cell j: i){
+		for (Cell[] i : cells) {
+			for (Cell j : i) {
 				j.selected = false;
 				j.added = false;
 			}
 		}
 	}
+
+	@Override
+	public String toString() {
+		String str = "";		
+		for (int row = 0; row < ROW_COUNT; row++) {
+			for (int col = 0; col < COL_COUNT; col++) {
+				str += cells[col][row].letter;				
+			}
+		}		
+		return str;
+	}
 	
-	Board active = null;
-	public Board  getActive(){
-		return active;
+	public void copy(Board b) {
+		for (int row = 0; row < ROW_COUNT; row++) {
+			for (int col = 0; col < COL_COUNT; col++) {
+				Position p = new Position(col, row);
+				
+				cells[col][row].copy(b.getCell(p));				
+			}
+		}
 	}
 
 }
