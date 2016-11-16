9126331851743085fa43b7d5084216c07f60cd76
quangvu <quangvuwpi@gmail.com>
2016-11-15 23:50:08 -0500

    Clean up code

--- WordSweeper/src/client/view/WordHistory.java
+++ WordSweeper/src/client/view/WordHistory.java
@@ -1,19 +1,24 @@
 package client.view;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
 import javax.swing.DefaultListModel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.ListCellRenderer;
+
 import client.model.Game;
+import client.model.Word;
 
 @SuppressWarnings("serial")
 public class WordHistory extends JPanel implements IBoundary {
 
-	JList list;
-	DefaultListModel model;
+	JList<Word> list;
+	DefaultListModel<Word> model;
 
 	final Game game;
 	Font myFont = new Font("Wawati SC", Font.PLAIN, 25);
@@ -32,12 +37,25 @@ public class WordHistory extends JPanel implements IBoundary {
 	 * set up word history view
 	 */
 	public boolean setup() {
-		// TODO Auto-generated method stub
 		setLayout(new BorderLayout());
-	    model = new DefaultListModel();
-	    list = new JList(model);
+	    model = new DefaultListModel<Word>();
+	    
+	    list = new JList<Word>(model);
 	    list.setFont(myFont);
 	    list.setSelectionInterval(-1, -1); // not selectable
+	    list.setCellRenderer(new ListCellRenderer<Word>() {
+			@Override
+			public Component getListCellRendererComponent(
+					JList<? extends Word> list, Word value, int index,
+					boolean isSelected, boolean cellHasFocus) {
+				
+				JTextArea area = new JTextArea(value.toString());
+				area.setFont(myFont);
+
+				return area;
+			}
+		});
+	    
 	    JScrollPane pane = new JScrollPane(list);
 		Dimension d = new Dimension(148, 249);
 		pane.setPreferredSize(d);
@@ -47,7 +65,7 @@ public class WordHistory extends JPanel implements IBoundary {
 
 	@Override
 	public boolean teardown() {
-		// TODO Auto-generated method stub
+		model.removeAllElements();
 		removeAll();
 		return true;
 	}
@@ -56,11 +74,9 @@ public class WordHistory extends JPanel implements IBoundary {
 	 * add word in word history
 	 * @param word
 	 */
-	public void addWord(String word) {
+	public void addWord(Word word) {
 		model.addElement(word);
 		list.ensureIndexIsVisible(model.size() - 1); // Auto scroll
-		this.validate();
-		this.repaint();
 	}
 
 	@Override
@@ -68,7 +84,10 @@ public class WordHistory extends JPanel implements IBoundary {
 	 * after add new word, refresh the word history board 
 	 */
 	public void refresh() {
-		// TODO Auto-generated method stub
-		
+		model.clear();
+		for (Word w : game.history) {
+			addWord(w);
+		}
+		repaint();
 	}
 }
