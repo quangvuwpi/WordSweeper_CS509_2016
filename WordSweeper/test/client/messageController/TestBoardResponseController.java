ac790e9b67814bd225674d572c5aeb790dff111f
quangvu <quangvuwpi@gmail.com>
2016-11-20 20:29:52 -0500

    Update package-level documentation

---
+++ WordSweeper/test/client/messageController/TestBoardResponseController.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package client.messageController;
+
+import java.util.UUID;
+
+import client.MockServerAccess;
+import client.model.Board;
+import client.model.Model;
+import client.model.Player;
+import client.model.Position;
+import client.view.Application;
+import junit.framework.TestCase;
+import response.BoardResponse;
+import response.PlayerResponse;
+import utility.XMLParser;
+import xml.Message;
+
+/**
+ * @author quangvu
+ *
+ */
+public class TestBoardResponseController extends TestCase {
+	
+	Application app;
+	Model model;
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		Message.configure("wordsweeper.xsd");
+		
+		model = new Model();
+		app = new Application(model);
+	}
+
+	/**
+	 * Test method for {@link client.messageController.BoardResponseController#process(xml.Message)}.
+	 */
+	public void testProcess() {
+		model.game.currentUser = "player0";
+		model.game.setBoard(XMLParser.parseXmlBoard("ACOWEFGHIJKLWALL"));
+		
+		String xmlString = Message.responseHeader(UUID.randomUUID().toString()) +
+				"<boardResponse gameId='hg12jhd' size='1' managingUser='player0' bonus='4,3' contents='ABCGBCJDH...HDJHJD'>" +
+			      "<player name='player0' score='392489038' position='4,6' board='AFERKSOEROIERPOR'/>" +
+			  "</boardResponse>" +
+			"</response>";
+		
+		BoardResponseController bc = new BoardResponseController(app, model);
+		bc.process(new Message(xmlString));
+
+		assertTrue(model.game.isManagingUser);
+		assertEquals(1, model.game.playerCount());
+		assertEquals("hg12jhd", model.game.gameId);
+		assertEquals("AFERKSOEROIERPOR", model.game.board.toString());
+		
+		//Player p0 = model.game.getPlayer(model.game.currentUser);
+		//assertEquals("player0", p0.name);
+		//assertEquals(392489038, p0.score);
+		//assertEquals(new Position(3,5), p0.position); // offset by 1
+		
+		
+	}
+
+}
